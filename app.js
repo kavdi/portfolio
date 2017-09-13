c2b7eba9412008222bb4261362ead458c033ba0b
kavdihodgson <kavdyjh@gmail.com>
2017-09-13 11:37:54 -0700

    got the constructor finished

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 'use strict';
 
-function Project (name, url, img, description) {
-  this.name = name;
-  this.url = url;
-  this.img = img;
-  this.description = description;
-}
-
-document.getElementById('menu_icon').addEventListener('click', reveal);
+document.getElementById('menu_button').addEventListener('click', reveal);
 var clicked = false;
 
 function reveal() {
@@ -20,4 +13,20 @@ function reveal() {
     nav.style.display = 'none';
     clicked = false;
   }
+}
+
+function Project (projectData) {
+  this.title = projectData.title;
+  this.projectUrl = projectData.url;
+  this.projectImg = projectData.img;
+  this.description = projectData.description;
+}
+
+Project.prototype.toHtml = function () {
+  var $newProject = $('.template').clone();
+  $newProject.attr('class', '');
+  $newProject.find('a').attr('href', this.projectUrl)
+                       .attr('src', this.projectImg)
+  $newProject.find('h1').text(this.title);
+  $newProject.find('p').text(this.description);
 }
--- projects.js
+++ projects.js
@@ -1,3 +1,9 @@
 'use strict';
 
-var workoutGenerator = new Project('Workout Generator', 'https://famavott.github.io/Home-Workout-Generator/', 'img/kettlebell.png', 'This is a look at the final  project for 201 Code Fellows. I collaborated with Matt Favoino, Robert Bronson and Brian Wheeler. The concept was to make an easy to use workout generator that can give you a good short workout for your specific needs.')
+var projectData = [
+  {title: 'Home Fit',
+    projectUrl: 'https://famavott.github.io/Home-Workout-Generator/',
+    projectImg: 'img/kettlebell.png',
+    description: 'This is a look at the final  project for 201 Code Fellows. I collaborated with Matt Favoino, Robert Bronson and Brian Wheeler. The concept was to make an easy to use workout generator that can give you a good short workout for your specific needs.'},
+  {}
+]

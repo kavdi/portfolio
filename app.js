fe95d73c7bdfa83d7599d5860cf3fd666e4882c5
kavdihodgson <kavdyjh@gmail.com>
2017-09-13 09:00:08 -0700

    menu drop down button added

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 'use strict';
 
-var workoutGenerator = new Project('Workout Generator', 'https://famavott.github.io/Home-Workout-Generator/', 'img/kettlebell.png', 'This is a look at the final  project for 201 Code Fellows. I collaborated with Matt Favoino, Robert Bronson and Brian Wheeler. The concept was to make an easy to use workout generator that can give you a good short workout for your specific needs.')
-
 function Project (name, url, img, description) {
   this.name = name;
   this.url = url;
   this.img = img;
   this.description = description;
+}
+
+document.getElementById('menu_icon').addEventListener('click', reveal);
+var clicked = false;
+
+function reveal() {
+  var nav = document.getElementById('nav');
+  if (!clicked){
+    nav.style.display = 'block';
+    clicked = true;
+  }
+  else if (clicked){
+    nav.style.display = 'none';
+    clicked = false;
+  }
 }
---
+++ projects.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var workoutGenerator = new Project('Workout Generator', 'https://famavott.github.io/Home-Workout-Generator/', 'img/kettlebell.png', 'This is a look at the final  project for 201 Code Fellows. I collaborated with Matt Favoino, Robert Bronson and Brian Wheeler. The concept was to make an easy to use workout generator that can give you a good short workout for your specific needs.')

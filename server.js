3a8138a4167d7f5929adf2828353dd3898e48d75
kavdihodgson <kavdyjh@gmail.com>
2017-09-27 11:26:19 -0700

    added dependencies

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const PG = require('pg');
+const FS = require('fs');
 const EX = require('express');
 const PRS = require('body-parser').urlencoded({extended: true});
 const app = EX();
 const PORT = process.env.PORT || 3000;
+const requestProxy = require('express-request-proxy');
 
 app.use(EX.static('public'));
 
